7cac450fb61358ed501f2b3af6713af4b491292c
jaisatyaabhiram <abhiramjaisatya@gmail.com>
2025-03-27 21:41:20 +0530

    adding views. lets design it later

--- index.js
+++ index.js
@@ -2,9 +2,14 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+app.set('view engine','ejs');
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
+
+app.get('/signup', (req, res) => {
+    res.render('signup');
+  })
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
